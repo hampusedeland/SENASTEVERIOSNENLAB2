7c584df2eb65d41b4f9306b19e06f6ceef713d37
Zannah Walker <zannah@student.chalmers.se>
2020-11-18 12:53:13 +0100

    ny ide

--- Lab1Testing/src/OrdinaryWorkshop.java
+++ Lab1Testing/src/OrdinaryWorkshop.java
@@ -1,14 +1,40 @@
 import java.util.ArrayList;
 
-
+//när man skapar ArrayListen så anger vad typvariabeln är och det påverkar hela klassen
+//C
 
     public class OrdinaryWorkshop<C extends Car>{
-        private C carType;
-        private ArrayList<C> loadedCars;
+        private C carType; /*typparameter som extendar Car, bara bilar kan läggas till
+        kan även ha en workshop som tar emot lastbil --> upperbound*/
+        private ArrayList<C> loadedCars = new ArrayList<C>();
+        //arraylist av typen C, defaultvärde
         private Integer maxCars;
 
+        //konstruktorn ett argument, max cars
         public OrdinaryWorkshop(int maxCars) {
             this.maxCars = maxCars;
+        }
+
+        public void addToloadedCars(C bil){
+            if (loadedCars.size() < maxCars) {
+                loadedCars.add(bil);
+            }
+        }
+public String removeFromLoadedCars(){
+            if (loadedCars.size()>0){
+                C furthestOutCar = loadedCars.get(loadedCars.size()-1);
+                loadedCars.remove(loadedCars.size()-1);
+                return (furthestOutCar.getClass().getName());
+            }
+            return "no cars";
+        }
+        public void lastaAvAllaBilar(){
+            while (loadedCars.size()>0){
+                removeFromLoadedCars();
+            }
+        }
+public Integer getNumberOfLoadedCars(){
+            return loadedCars.size();
         }
 
 
